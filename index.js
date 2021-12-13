679a8da15e20687c4fdf8408729d19f1972d17e9
Kseniia McFadden <mcfaddenkseniia@gmail.com>
2021-12-13 15:01:03 -0800

    completed assignment

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
-const dodger = document.getElementById("dodger");
+const dodger = document.getElementById('dodger')
 
-function moveDodgerLeft() {
+  document.addEventListener("keydown", e => {
+    if (e.key === "ArrowLeft"){
+      moveDodgerLeft();
+    }
+    else if (e.key === "ArrowRight"){
+      moveDodgerRight();
+    }
+  })
+  
+  function moveDodgerLeft() {
     const leftNumbers = dodger.style.left.replace("px", "");
     const left = parseInt(leftNumbers, 10);
   
@@ -9,22 +18,11 @@ function moveDodgerLeft() {
     }
   }
 
-  document.addEventListener("keydown", function (e) {
-    if (e.key === "ArrowLeft") {
-      moveDodgerLeft();
-    }
-  });
 
-function moveDodgerRight() {
-    const leftNumbers = dodger.style.left.replace("px", "");
-    const left = parseInt(leftNumbers, 10);
-  
-    if (left < 360) {
+function moveDodgerRight(){
+  const leftNumbers = dodger.style.left.replace("px", "");
+  const left = parseInt(leftNumbers, 10);
+    if (left < 360){
       dodger.style.left = `${left + 1}px`;
     }
-  } 
-  document.addEventListener("keydown", function (e) {
-    if (e.key === "ArrowRight") {
-        moveDodgerRight();
-    }
-  });
+}
